30fe870f9fa8b64445fb088c05316372f8d6e1aa
diptoislam2006-svg <diptoislam2006@gmail.com>
2026-01-21 22:42:21 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,21 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
+export default defineConfig({
+  plugins: [react()],
+    base:"/y/"
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // This splits your dependencies into a separate 'vendor' file
+            return 'vendor';
+          }
+        },
       },
-      plugins: [react()],
-       base: '/p/',
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+    },
+    // Optional: increase the limit slightly if you prefer 
+    // chunkSizewarningLimit: 1000, 
+  },
+})
